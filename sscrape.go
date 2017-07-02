80a37ce3ae22f800203f80999684506b17a6ba7e
Norblad, Walt <walt.norblad@cengage.com>
2017-07-01 22:26:51 -0700

    Added some comments filled in a couple functions

--- sscrape.go
+++ sscrape.go
@@ -3,21 +3,91 @@ package sscrape
 import (
 	"fmt"
 	"net/http"
-	"log"
 	"bytes"
-	"strconv"
 	"net/url"
 	"strings"
-	"os"
+	"io/ioutil"
 )
 
-
+// TargetServer represents the server to be scraped
 type TargetServer struct {
+	// Jar (optional) is the cookie jar used to hold session cookies and other cookies. Normally this is filled
+	// with the LoginFunction. It's only really used for cookie based session tracking.
 	Jar []*http.Cookie
+
+	// AgentName (optional) is a fun little morsel meant just to give your script a name while it scrapes
+	// sites. It's only really used for fun, though I guess you could masquerade as real browsers
+	// with it. But why?
+	AgentName string
+
+	// Host (required) is the fully qualified URL of the server to be scraped
+	// For example: http://example.com:8080 or http://google.com
 	Host string
-	ConfLoc string
+
+	// SessionCookieName (optional) is the name of the session cookie the target server uses.
+	// Providing this cookie name will make Form based logins easier as it will error if the
+	// appropriate cookie is not found. Otherwise, it's not used.
+	SessionCookieName string
 }
 
+// FormLogin tries to log into the site using the login parameters provided in the form parameter.
+// Any cookies returned will be stored in the Jar. If a SessionCookieName value is set, this function
+// will look for it in the response in particular. If it's not found then an error is returned. This
+// is useful for determining if a login was actually successful
+func (ts *TargetServer) FormLogin(loginPath string, form url.Values) error {
+	req, err := ts.Request("POST", loginPath, form)
+	if err != nil {
+		return fmt.Errorf("unable to create log in request, %v", err)
+	}
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("unable to make request to target host due to %v", err)
+	}
+
+	if ts.SessionCookieName != "" && ts.HasCookie(resp.Cookies(), ts.SessionCookieName) {
+		ts.Jar = resp.Cookies()
+	} else {
+		return fmt.Errorf("session cookie not found after login due to possible bad username and password")
+	}
+
+	return nil
+}
+
+
+// GetPage gets a page from the server and returns the result as a string. At the moment
+// there is no support for binary results.
+func (ts *TargetServer) GetPage(path string, query url.Values) (string, error) {
+	req, err := ts.Request("GET", path, query)
+	if err != nil {
+		return "", fmt.Errorf("unable to create log in request, %v", err)
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("unable to make request to target host due to %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to read body from response, %v", err)
+	}
+
+	return  string(body), nil
+}
+
+
+// URL is a helper function that just builds the right urls for me because using net/url
+// is tedious.
 func (ts TargetServer) URL(path string) (string, error) {
 	u, err := url.ParseRequestURI(ts.Host)
 	if err != nil {
@@ -28,23 +98,44 @@ func (ts TargetServer) URL(path string) (string, error) {
 	return u.String(), nil
 }
 
-func (ts TargetServer) PostRequest(path string, form url.Values) (*http.Request, error) {
+// Reqest build a request and adds common thing that I always end up adding. More importantly
+// this loads the cookies into the request
+func (ts TargetServer) Request(method string, path string, form url.Values) (*http.Request, error) {
 	url, err := ts.URL(path)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(form.Encode()))
+	req, err := http.NewRequest(method, url, bytes.NewBufferString(form.Encode()))
 	if err != nil {
 		return nil, fmt.Errorf("invalid request, unable to construct: %v", err)
 	}
 
-	req.Header.Set("User-Agent", "SScraper/1.0")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// While this function only accepts text reponses, I don't want to list them at the moment.
+	// one day when I feel keep on it, I'll update this with actual accept types.
+	req.Header.Set("Accept", "*/*")
+
+	if ts.AgentName == "" {
+		req.Header.Set("User-Agent", "SScraper/1.0")
+	} else {
+		req.Header.Set("User-Agent", ts.AgentName)
+	}
+
+	if method != "GET" || method != "HEAD" {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	for _, cookie := range(ts.Jar) {
+		req.AddCookie(cookie)
+	}
 
 	return req, nil
 }
 
+// SwapsCookies swaps a cookie in a list of cookies. This was supposed to be used to refresh the
+// Session cookie if it were dynamic, but then it turned out to just be easier to dump all of the
+// cookies in to the jar. One day, I plan on getting back into using this and dealing with cookies
+// with intelligence rather than a caveman club.
 func (ts TargetServer) SwapCookies(foundCookies []*http.Cookie, cookiePrefix string) {
 	newJar := make([]*http.Cookie, len(ts.Jar), len(ts.Jar)+1)
 
@@ -65,6 +156,7 @@ func (ts TargetServer) SwapCookies(foundCookies []*http.Cookie, cookiePrefix str
 	ts.Jar = newJar
 }
 
+// HasCookie checks a list of cookies if a cooke is there by name.
 func (ts TargetServer) HasCookie(cookies []*http.Cookie, cookiePrefix string) bool {
 	for _, cookie := range(cookies) {
 		if strings.HasPrefix(cookie.Name, cookiePrefix) {
@@ -74,7 +166,3 @@ func (ts TargetServer) HasCookie(cookies []*http.Cookie, cookiePrefix string) bo
 
 	return false
 }
-
-
-
-
